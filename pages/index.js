1bb87d07eed093c18818664590a0dbf8a874a217
Maryam Shahvalizadeh <Shahvalizadehmaryam@gmail.com.>
2024-09-28 11:49:58 +0330

    added login handler function .

--- pages/index.js
+++ pages/index.js
@@ -2,8 +2,14 @@ import Head from "next/head";
 import Image from "next/image";
 import styles from "../styles/Home.module.css";
 import Link from "next/link";
+import { useRouter } from "next/router";
 
 export default function Home() {
+  const router = useRouter();
+
+  const loginHandler = () => {
+    router.push("/users");
+  };
   return (
     <div className={styles.container}>
       <h3>Home page</h3>
@@ -13,6 +19,7 @@ export default function Home() {
       <Link href={`/users`}>
         <p>Go to users</p>
       </Link>
+      <button onClick={loginHandler}>Log in</button>
     </div>
   );
 }
