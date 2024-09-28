983213f9b205121934cc9fdd222be498c301322b
Maryam Shahvalizadeh <Shahvalizadehmaryam@gmail.com.>
2024-09-28 10:56:04 +0330

    i removed header and footer from app.js file.

--- pages/_app.js
+++ pages/_app.js
@@ -3,9 +3,9 @@ import "../styles/globals.css";
 function MyApp({ Component, pageProps }) {
   return (
     <>
-      <p>Header</p>
+      {/* <p>Header</p> */}
       <Component {...pageProps} />
-      <p>footer</p>
+      {/* <p>footer</p> */}
     </>
   );
 }
